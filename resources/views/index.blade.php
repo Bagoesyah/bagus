d99b8c26d063898a3b4224d03933ce5623bbdfae
Bagoesyah <bnurmansyah8@gmail.com>
2022-07-06 17:23:15 +0700

    percobaan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,9 +6,11 @@
     <title>Bootstrap demo</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
-    <div class="container">
-        <div class="content"></div>
+      <div class="container">
+          <div class="content">
+              <h1>Hello, world!</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae amet libero cupiditate, animi repellat aperiam distinctio quos sequi, a natus odit perferendis, soluta nihil ipsam illo quidem aut esse similique?</p>
+        </div>
     </div>
   </body>
 </html>
